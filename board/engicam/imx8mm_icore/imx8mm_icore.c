481cc99364bce02e50b47f0c25482c36a51e71d6
Domenico Acri <domenico.acri@engicam.com>
2022-09-29 11:35:28 +0200

    imx8mm icore: added support for ksz9021/ksz9031

--- board/engicam/imx8mm_icore/imx8mm_icore.c
+++ board/engicam/imx8mm_icore/imx8mm_icore.c
@@ -39,11 +39,18 @@ static iomux_v3_cfg_t const wdog_pads[] = {
 
 #define FEC_RST_PAD IMX_GPIO_NR(3, 7)
 #define MDIOAADR2 IMX_GPIO_NR(1, 25)
+#define FEC_RST_PAD_KSZ9021 IMX_GPIO_NR(2, 9)
+#define RGMII_RXDV IMX_GPIO_NR(1, 24)
 static iomux_v3_cfg_t const fec1_rst_pads[] = {
         IMX8MM_PAD_NAND_DATA01_GPIO3_IO7 | MUX_PAD_CTRL(NO_PAD_CTRL),
 		IMX8MM_PAD_ENET_RXC_GPIO1_IO25 | MUX_PAD_CTRL(NO_PAD_CTRL),
+        IMX8MM_PAD_SD1_DATA7_GPIO2_IO9 | MUX_PAD_CTRL(NO_PAD_CTRL),
+        IMX8MM_PAD_ENET_RX_CTL_GPIO1_IO24 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
+static iomux_v3_cfg_t const fec1_rgmii_rxdv_pads[] = {
+        IMX8MM_PAD_ENET_RX_CTL_ENET1_RGMII_RX_CTL | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
 
 int board_early_init_f(void)
 {
@@ -62,14 +69,19 @@ int board_early_init_f(void)
 
 	gpio_request(FEC_RST_PAD, "fec1_rst");
 	gpio_request(MDIOAADR2, "mdioaddr2");
+	gpio_request(RGMII_RXDV, "rgmii_rxdv");
 
 	udelay(1000);
 	gpio_direction_output(MDIOAADR2, 0);
-	udelay(5000);
 	gpio_direction_output(FEC_RST_PAD, 0);
-	udelay(5000);
+	gpio_direction_output(RGMII_RXDV, 0);
+	gpio_direction_output(FEC_RST_PAD_KSZ9021, 0);
+	udelay(10000);
 	gpio_direction_output(FEC_RST_PAD, 1);
-	udelay(5000);
+	gpio_direction_output(FEC_RST_PAD_KSZ9021, 1);
+	udelay(100);
+
+	imx_iomux_v3_setup_multiple_pads(fec1_rgmii_rxdv_pads, ARRAY_SIZE(fec1_rgmii_rxdv_pads));
 
 	return 0;
 }
@@ -88,6 +100,50 @@ static int setup_fec(void)
 
 }
 
+#define MICREL_KSZ9021_EXTREG_CTRL	0xB
+#define MICREL_KSZ9021_EXTREG_DATA_WRITE	0xC
+#define MICREL_KSZ9021_RGMII_CLK_CTRL_PAD_SCEW	0x104
+#define MICREL_KSZ9021_RGMII_RX_DATA_PAD_SCEW	0x105
+
+void ksz9021rn_phy_fixup(struct phy_device *phydev)
+{
+	printf("KSZ9021\n");
+	phy_write(phydev, MDIO_DEVAD_NONE, MICREL_KSZ9021_EXTREG_CTRL, 0x8000 | MICREL_KSZ9021_RGMII_RX_DATA_PAD_SCEW);
+	phy_write(phydev, MDIO_DEVAD_NONE, MICREL_KSZ9021_EXTREG_DATA_WRITE, 0x0000);
+
+	phy_write(phydev, MDIO_DEVAD_NONE, MICREL_KSZ9021_EXTREG_CTRL, 0x8000 | MICREL_KSZ9021_RGMII_CLK_CTRL_PAD_SCEW);
+	phy_write(phydev, MDIO_DEVAD_NONE, MICREL_KSZ9021_EXTREG_DATA_WRITE, 0xf0f0);
+
+	phy_write(phydev, MDIO_DEVAD_NONE, MICREL_KSZ9021_EXTREG_CTRL, MICREL_KSZ9021_RGMII_CLK_CTRL_PAD_SCEW);
+}
+
+void mmd_write_reg(struct phy_device *phydev, int device, int reg, int val)
+{
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x0d, device);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x0e, reg);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x0d, (1 << 14) | device);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x0e, val);
+}
+
+static int ksz9031rn_phy_fixup(struct phy_device *phydev)
+{
+	printf("KSZ9031\n");
+
+	//write register 6 addr 2 TXD[0:3] skew
+	mmd_write_reg(phydev, 2, 6, 0x4111);
+
+	//write register 5 addr 2 RXD[0:3] skew
+	mmd_write_reg(phydev, 2, 5, 0x47a7);
+
+	//write register 4 addr 2 RX_DV TX_EN skew
+	mmd_write_reg(phydev, 2, 4, 0x004A);
+
+	//write register 8 addr 2 RX_CLK GTX_CLK skew
+	mmd_write_reg(phydev, 2, 8, 0x0273);
+
+	return 0;
+}
+
 int board_phy_config(struct phy_device *phydev)
 {
 	/* enable rgmii rxc skew and phy mode select to RGMII copper */
@@ -98,6 +154,22 @@ int board_phy_config(struct phy_device *phydev)
 	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x82ee);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x05);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x100);
+
+	unsigned short tmp = 0;
+
+	if (miiphy_read("FEC0", CONFIG_FEC_MXC_PHYADDR, MII_PHYSID2, &tmp) != 0)
+		debug("PHY ID register 3 read failed\n");
+
+	unsigned short model = (tmp>>4) & 0x3F;
+
+	if (model == 0x21) // KSZ9021
+	{
+		ksz9021rn_phy_fixup(phydev);
+	}
+	else if (model == 0x22) // KSZ9031
+	{
+		ksz9031rn_phy_fixup(phydev);
+	}
 
 	if (phydev->drv->config)
 		phydev->drv->config(phydev);
